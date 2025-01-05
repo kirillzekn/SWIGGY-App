34c762c89d425ad27e9e14a287d3d55f7b5fe2c9
Kirill <Kirill@Kirill.com>
2025-01-05 13:23:33 +0500

    adding secret values

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "ARM_TENANT_ID" {
+  description = "Azure Tenant ID"
+  type        = string
+  sensitive = true
+}
+
+variable "ARM_SUBSCRIPTION_ID" {
+  description = "Azure Subscription ID"
+  type        = string
+  sensitive = true
+}
