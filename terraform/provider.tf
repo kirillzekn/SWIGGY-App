dc2f09179571cd1f8679c4a4124695b4ad78ed47
Kirill <Kirill@Kirill.com>
2025-01-08 15:36:53 +0500

    vnet change

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -22,17 +22,3 @@ provider "azurerm" {
   subscription_id = var.ARM_SUBSCRIPTION_ID
   tenant_id       = var.ARM_TENANT_ID
 }
-
-# Create a resource group
-resource "azurerm_resource_group" "default" {
-  name     = "swiggy-app-rg"
-  location = "West Europe"
-}
-
-# Create a virtual network within the resource group
-resource "azurerm_virtual_network" "default" {
-  name                = "swiggy-app-network"
-  resource_group_name = azurerm_resource_group.default.name
-  location            = azurerm_resource_group.default.location
-  address_space       = ["10.0.0.0/16"]
-}
