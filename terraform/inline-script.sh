232f0a625df403b9a50527f6ca6c77f17711fd7e
Kirill <Kirill@Kirill.com>
2025-01-09 12:56:20 +0500

    updated inline script with DOcker related details

--- terraform/inline-script.sh
+++ terraform/inline-script.sh
@@ -7,11 +7,32 @@ sudo apt-get install trivy  -y
 
 
 #install Docker
-sudo apt-get update
-sudo apt-get install docker.io -y
-sudo usermod -aG docker ubuntu
-sudo newgrp docker
-sudo chmod 777 /var/run/docker.sock
+# sudo apt-get update
+# sudo apt-get install docker.io -y
+# sudo usermod -aG docker ubuntu
+# sudo newgrp docker
+# sudo chmod 777 /var/run/docker.sock
+
+#install Docker
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Add Docker's official GPG key:
+sudo apt-get update -7
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+
 
 #create homedir for jenkins
 #sudo mkdir -p /var/jenkins_home
