c06de67e4e64e2772fafed843f556741358d2de2
Kirill <Kirill@Kirill.com>
2025-01-09 13:18:52 +0500

    updated version of ubuntu

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_machine" "default" {
     storage_image_reference {
         publisher = "Canonical"
         offer     = "UbuntuServer"
-        sku       = "22_04-lts"
+        sku       = "22.04-LTS" 
         version   = "latest"
     }
     storage_os_disk {
