50c38d6e4bbf508a9017ce542fadb89b0214e62a
Kirill <Kirill@Kirill.com>
2025-01-21 15:16:23 +0500

    output section

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,13 +1,3 @@
-
-data "azurerm_public_ip" "default" {
-    name                = azurerm_public_ip.default.name
-    resource_group_name = azurerm_resource_group.default.name
-    id          = azurerm_public_ip.default.id
-}
-
 output "azurerm_public_ip" {
-    value = {
-        name       = data.azurerm_public_ip.default.name
-        ip_address = data.azurerm_public_ip.default.id
-    }  
+    value = "${azurerm_public_ip.default.*.ip_address}"
 }
